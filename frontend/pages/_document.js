42e1561e814fc9f269e99b040998d05493996aec
Nicole <nicolemamay4@gmail.com>
2021-03-27 21:54:50 -0400

    added progress bar

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,9 +1,24 @@
-import Document, { Html, NextScript, Main } from 'next/document';
+import Document, { Html, Head, NextScript, Main } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 
 class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    /* Goes through every component that needs to be rendered to the page
+     * checks if there's any styled components, grabs those styles
+     * and renders them out to the server
+     */
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en-US">
+        <Head />
         <body>
           <Main />
           <NextScript />
